8e2c9468e47507c0c14478cd01bfa3bffda74c5d
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-04-06 19:11:17 +0530

    changes in Faculty Interface

--- src/pages/facultyInterface.tsx
+++ src/pages/facultyInterface.tsx
@@ -1,10 +1,13 @@
 import Profile from "../../components/facultyResource/profile";
 import Pd from "../../components/facultyResource/profileData";
 import MoreInfo from "../../components/facultyResource/moreInfo";
+import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer";
 
 const FacultyInterface = () => {
   return (
     <div>
+      <Navbar />
       <div className="flex justify-center items-center gap-40 border-4 mx-12 mb-16 max-xl:flex-col">
         <div>
           <Profile />
@@ -14,6 +17,7 @@ const FacultyInterface = () => {
         </div>
       </div>
       <MoreInfo />
+      <Footer />
     </div>
   );
 };
