68c87614858f60a914f026b3e8592e40f2c1aa0f
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-04-10 20:26:17 +0530

    changes in faculty interface

--- components/herosection/welcome.tsx
+++ components/herosection/welcome.tsx
@@ -1,14 +1,41 @@
 import Link from "next/link";
 import { MdNotificationsNone } from "react-icons/md";
 import { DiMagento } from "react-icons/di";
+import { BsSearch } from "react-icons/bs";
 
 const Welcome = () => {
   return (
-    <div className="flex justify-between sm:mx-14 h-10 items-center max-sm:ml-6">
+    <div className="flex justify-between sm:mx-14 h-14 items-center max-sm:ml-6 my-4">
       <div className="text-4xl font-semibold  max-sm:text-2xl flex flex-row justify-center items-center gap-4">
         <DiMagento className="h-8 w-8" />
         Welcome back,
         <span className="tracking-wider text-[#ff5901]">Vadivu!</span>
+      </div>
+      <div>
+        <div className="flex items-center justify-between flex-row rounded-lg border-2 px-4 py-2">
+          <div className="">
+            <select className="text-base text-gray-800 outline-none  ">
+              <option>All</option>
+              <option>Expert Name</option>
+              <option>Organization</option>
+              <option>Designation</option>
+              <option>Expertise</option>
+              <option>Honour Awards</option>
+              <option>Web of Science</option>
+            </select>
+          </div>
+          <div className="flex justify-center items-center flex-row ">
+            <input
+              className="text-base text-gray-400 flex-grow outline-none px-4 border-l-2 ml-4 w-96"
+              type="search"
+              placeholder="Search for.. "
+            />
+
+            <button className="outline-none">
+              <BsSearch />
+            </button>
+          </div>
+        </div>
       </div>
       <Link href="">
         <MdNotificationsNone className="h-8 w-8" />
--- src/pages/facultyInterface.tsx
+++ src/pages/facultyInterface.tsx
@@ -4,13 +4,14 @@ import MoreInfo from "../../components/facultyResource/moreInfo";
 import Navbar from "../../components/navbar/navbar";
 import Footer from "../../components/footer";
 import Welcome from "../../components/herosection/welcome";
+import Image from "next/image";
+import graph from "../../public/assets/gip.gif";
 
 const FacultyInterface = () => {
   return (
     <div>
-      <Navbar />
       <Welcome />
-      <div className="flex justify-center items-center gap-40 border-4 mx-12 mb-16 max-xl:flex-col mt-4">
+      <div className="flex justify-center items-center gap-40 border-4 mx-12 mb-8 max-xl:flex-col mt-4">
         <div>
           <Profile />
         </div>
@@ -18,7 +19,9 @@ const FacultyInterface = () => {
           <Pd />
         </div>
       </div>
-
+      <div className="flex justify-center items-center border-4 mx-12 mb-8 max-xl:flex-col mt-4">
+        <Image src={graph} alt="" className="h-[600px] w-full" />
+      </div>
       <Footer />
     </div>
   );
